735be5d19cf28da1a2b247ec5616059f32e5396d
VorontsovaOV <u211-05@public.mpei.local>
2017-03-30 15:27:11 +0300

    add enter a & b

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+using namespace std;
 
 int
 main() {
-    std::cout << "Hello, World!" << std::endl;
+    int a, b;
+    cout << "Enter A and B: ";
+    cin >> a >> b;
     return 0;
 }
